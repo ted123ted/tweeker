57670fb0e8b17d0bd94ad2fe5db321d7706cf0d8
ted123ted <51775702+ted123ted@users.noreply.github.com>
2021-06-16 11:01:33 +0300

    Add files via upload

---
+++ tweeker/profiles/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    follows = models.ManyToManyField('self', related_name='followed_by', symmetrical=False)
+
+
+User.profile = property(lambda u: Profile.objects.get_or_create(user=u)[0])
+
+
